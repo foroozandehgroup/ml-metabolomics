a24df1a1215c98f3b27daebced76bdbc8f72f36c
Daniel Alimadadian <danielalimadadian01@gmail.com>
2022-11-17 15:21:25 +0000

    Include ranked loadings, quantiles, new plots.

--- pca/PCA_2class.py
+++ pca/PCA_2class.py
@@ -7,6 +7,7 @@ from matplotlib.lines import Line2D
 from sklearn.decomposition import PCA
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.preprocessing import StandardScaler
+from scipy import stats
 from pyopls import OPLS
 import pickle
 from pathlib import Path
@@ -21,15 +22,15 @@ class Data:
     def new_from_csv(cls, fname: str) -> Data:
 
         # parse data into a pandas DataFrame
-        test_data = pd.read_csv(f'{fname}')
-        ids = test_data.loc[:, 'ID'].to_numpy()
-        classes = test_data.loc[:, 'Class'].to_numpy()
+        cls.test_data = pd.read_csv(f'{fname}')
+        ids = cls.test_data.loc[:, 'ID'].to_numpy()
+        classes = cls.test_data.loc[:, 'Class'].to_numpy()
         integs = [
-            test_data.iloc[i,2:].to_numpy() 
-            for i in range(0, len(test_data))
+            cls.test_data.iloc[i,2:].to_numpy() 
+            for i in range(0, len(cls.test_data))
         ]
 
-        labels = list(test_data.columns.values)[2:]
+        labels = list(cls.test_data.columns.values)[2:]
 
         entries = [
             Entry(id_, class_, integ)
@@ -51,7 +52,7 @@ class Data:
                     return entry.integs    
     ### raise exception if id is not in entry ids
 
-    def set_dataset_classes(self, control: str, case: str, class_labels: dict=None):
+    def set_dataset_classes(self, control: str, case: str, class_labels: dict=None, sort=False):
         """
         Sets control and case for the dataset (currently 1 control and 1 case supported).
         Takes optional input class_labels (dict, keys: 'control', 'case') 
@@ -68,11 +69,19 @@ class Data:
                     entry.class_ = class_labels['control']
                 else:
                     entry.class_ = class_labels['case']
+            
+            for i in range(len(self.entries)):
+                if self.test_data.loc[:,'Class'].iloc[i] == self.control:
+                    self.test_data.loc[:,'Class'].iloc[i] = class_labels['control']
+                else:
+                    self.test_data.loc[:,'Class'].iloc[i] = class_labels['case']
 
             self.control = class_labels['control']
             self.case = class_labels['case']
+            ### is it necessary to change the attributes to the new values as well?
         
-        #self.entries = sorted(self.entries, key=lambda entry: entry.class_)
+        if sort:
+            self.entries = sorted(self.entries, key=lambda entry: entry.class_)
     
     def _split_data(self, keep_id: bool=False):
         """
@@ -90,16 +99,34 @@ class Data:
         y_data = np.array([entry.class_ for entry in self.entries])
 
         return x_data, y_data
+
+    def _scale_data(self):
+        """
+        Scales data: currently only standard scaling supported (zero mean and unit variance).
+        Initialises scaled_test_data attribute, which presents scaled data as a pandas 
+        DataFrame (same form as test_data).
+        """
+        if self.scaled_data is None:
+            x_data, y_data = self._split_data()
+            self._scaled_data = StandardScaler().fit_transform(x_data)
+            scaled_test_data = pd.DataFrame(self._scaled_data, columns=self.labels)
+            scaled_test_data.insert(0, column='Class', value=y_data)
+            scaled_test_data.insert(0, column='ID', value=[entry.id for entry in self.entries])
+            self.scaled_test_data = scaled_test_data
+
+        return self.scaled_data
+        ### allow option for scaling method
+    
+    @property
+    def scaled_data(self):
+        return getattr(self, "_scaled_data", None)
             
     def _get_pcs(self, n_components):
         if self.pca is None:
-            x_data, y_data = self._split_data()
-            scaled_data = StandardScaler().fit_transform(x_data)
             self._pca = PCA(n_components=n_components)
-            self._pca.fit_transform(scaled_data)
+            self._pca.fit_transform(self._scale_data())
 
         return self.pca
-        ### allow option for scaling method
 
     def get_loadings(self, n_components: int=2):
         """
@@ -160,9 +187,12 @@ class Data:
         
         #quantiles for PC1/PC2 currently
         pc1, pc2 = [j[0] for i, j in self.loadings], [j[1] for i, j in self.loadings]
+        
+        self._upper_quantile_pc1 = np.quantile(pc1, q)
+        self._lower_quantile_pc1 = np.quantile(pc1, 1-q)
 
         self.pc1_loadings = [
-            (i, np.array([j[0], j[1]])) for i, j in self.loadings if j[0] > np.quantile(pc1, q) or j[0] < np.quantile(pc1, 1-q)
+            (i, np.array([j[0], j[1]])) for i, j in self.loadings if j[0] > self._upper_quantile_pc1 or j[0] < self._lower_quantile_pc1
             ]
         self.pc2_loadings = [
             (i, np.array([j[0], j[1]])) for i, j in self.loadings if j[1] > np.quantile(pc2, q) or j[1] < np.quantile(pc2, 1-q)
@@ -170,14 +200,63 @@ class Data:
         
         self.sig_loadings = self.pc1_loadings + self.pc2_loadings
         
-        sig_loadings_labels = [i[0] for i in self.sig_loadings]
+        self._sig_loadings_labels = [i[0] for i in self.sig_loadings]
         labels_column = [
-            label if label in sig_loadings_labels else "" for label in self.labels
+            label if label in self._sig_loadings_labels else "" for label in self.labels
             ]
 
         loadings_matrix['Labels'] = np.array(labels_column).T
 
         return loadings_matrix
+    
+    def rank_loadings(self):
+        """
+        Sorts loadings from smallest to largest. Sorts based off the value for PC1.
+        """
+        self.ranked_loadings = sorted(self.loadings, key= lambda x: x[1][0])
+        return self.ranked_loadings
+    
+    def get_sig_data(self):
+        """
+        Returns (and initialises an attribute self.sig_data) a pandas DataFrame
+        with all entries but only at significant peak frequencies given by the
+        upper and lower quantiles. 
+        """
+        self.pc1_data = self.test_data.loc[:, ['ID', 'Class'] + [j[0] for j in self.pc1_loadings]]
+        self.pc2_data = self.test_data.loc[:, ['ID', 'Class'] + [j[0] for j in self.pc2_loadings]]
+
+        self.sig_data = self.test_data.loc[:,['ID', 'Class'] + self._sig_loadings_labels]
+
+        return self.sig_data
+    
+    def _run_ttests(self, data):
+        """
+        Takes pandas DataFrame of test data as input (e.g. pc1_data). Returns DataFrame of p_values and significance
+        levels for each label (i.e. frequency range).
+        """
+
+        # initialise p_values DataFrame populated with zeros
+        #p_values = pd.DataFrame(np.zeros((len(data.columns[2:]), 3)), columns=['p-value', 'Significance', 'Bonferroni'], index=[data.columns[2:]])
+
+        ### THIS IS NOT USING SCALED DATA
+
+        for i, col in enumerate(data.columns[2:]):
+
+            # initialise DataFrame for mean, SD, and SEM for each column
+            stat = pd.DataFrame(np.zeros((2, 3)), columns=['Mean', 'SD', 'SEM'], index=[self.control, self.case])
+
+            for class_ in [self.control, self.case]:
+                pass
+
+            if i == 0:
+                p_value = stats.ttest_1samp(data.loc[:, col], popmean=0)
+                p_values = pd.DataFrame([np.array([p_value[1], 0, 0])], columns=['p-value', 'Significance', 'Bonferroni'], index=[col])
+            else:
+                p_value = stats.ttest_1samp(data.loc[:, col], popmean=0)
+                p_values.loc[col] = np.array([p_value[1], 0, 0])
+
+
+        return stat
 
     def plot_loadings(self, loadings_matrix: pd.DataFrame, sig_labels: bool=True):
         """
@@ -197,8 +276,20 @@ class Data:
             ax.scatter(loadings_matrix['PC1'], loadings_matrix['PC2'], color='black', alpha=0.5)
 
         self._add_labels_loadings(ax)
+    
+    def plot_ranked_loadings(self):
+        fig, ax = plt.subplots()
+        x_points = [i+1 for i, entry in enumerate(self.ranked_loadings)]
+        y_points = [entry[1][0] for entry in self.ranked_loadings]
 
-        ### add axes labels, add gridlines, add datapoint labels by id
+        ax.scatter(x_points, y_points, c='black', alpha=0.5)
+        ax.set_title('Ranked Loadings')
+        ax.set_xlabel('Rank')
+        ax.set_ylabel('PC1 Loadings')
+
+        ## quantile threshold lines
+        ax.plot(x_points, [self._upper_quantile_pc1 for i in x_points], c='red', linestyle='--')
+        ax.plot(x_points, [self._lower_quantile_pc1 for i in x_points], c='red', linestyle='--')
     
     def plot_scores(self, scores_matrix: pd.DataFrame):
         """
@@ -207,6 +298,7 @@ class Data:
         """
         colors = []
         # get class values from numpy to speed up run time
+        id_labels = scores_matrix.index
         np_scores = scores_matrix.to_numpy()
 
         for class_ in np_scores[:,0]:
@@ -225,20 +317,23 @@ class Data:
             handle.set_linestyle("")
 
         if self.n_components == 2:
-            self._plot_2d_scores(np_scores=np_scores, colors=colors, legend_elements=legend_elements)    
+            self._plot_2d_scores(np_scores=np_scores, colors=colors, legend_elements=legend_elements, id_labels=id_labels)    
         elif self.n_components >= 3:
-            self._plot_3d_scores(np_scores=np_scores, colors=colors, legend_elements=legend_elements)
+            self._plot_3d_scores(np_scores=np_scores, colors=colors, legend_elements=legend_elements, id_labels=id_labels)
     
-    def _plot_2d_scores(self, np_scores, colors, legend_elements):
+    def _plot_2d_scores(self, np_scores, colors, legend_elements, id_labels):
         fig, ax = plt.subplots()
         ax.scatter(np_scores[:, 1], np_scores[:, 2], c=colors, edgecolors='black', alpha=0.5)
+        for i, id in enumerate(id_labels):
+            ax.annotate(id, (np_scores[i, 1], np_scores[i, 2]))
         self._add_labels_scores(ax, legend_elements)
         
-    def _plot_3d_scores(self, np_scores, colors, legend_elements):
+    def _plot_3d_scores(self, np_scores, colors, legend_elements, id_labels):
         fig= plt.figure()
         ax = fig.add_subplot(projection='3d')
         ax.scatter(np_scores[:, 1], np_scores[:, 2], np_scores[:, 3], c=colors, edgecolors='black', alpha=0.5)
         self._add_labels_scores(ax, legend_elements)
+        ### add text labels for 3d plot
     
     @staticmethod
     def _add_labels_scores(ax, legend_elements):
@@ -284,7 +379,7 @@ class Entry:
     class_: str
     integs: np.ndarray
 
-test_data = Data.new_from_csv(r"C:\Users\mfgroup\Documents\Daniel Alimadadian\R Scripts\PCA\test_data.csv")
+test_data = Data.new_from_csv(r"C:\Users\mfgroup\Documents\Daniel Alimadadian\Metabolomics_ML\test_data.csv")
 
 # plots don't work without class_labels dict - cannot append strings into numpy array
 test_data.set_dataset_classes(control='SPMS', case='RRMS', class_labels={'control': -1, 'case': 1})
@@ -294,15 +389,16 @@ loadings_matrix, scores_matrix, vars_array = test_data.get_loadings(n_components
 
 quantiles_matrix = test_data.get_quantiles(loadings_matrix)
 
+test_data.rank_loadings()
+test_data.get_sig_data()
 
+print(test_data._run_ttests(test_data.pc1_data))
+
+#test_data.plot_ranked_loadings()
 #test_data.plot_vars(vars_array=vars_array, threshold=0.95, cumulative=True)
-test_data.plot_loadings(quantiles_matrix, sig_labels=True)
+#test_data.plot_loadings(quantiles_matrix, sig_labels=True)
 #test_data.plot_scores(scores_matrix)
 
-#print(test_data.get_quantiles())
-
-plt.show()
- 
 
 ### TO DO
 # check out quantiles and ranked loadings
@@ -310,13 +406,15 @@ plt.show()
 # create file with questions + next steps
 
 #### General rule - using get methods outputs pandas dataframes, but attributes use numpy arrays
-
+### ttests in pandas dataframes due to use of strings e.g. significance level
 
 # plot spectrum
 #fig, ax = plt.subplots()
 #x_vals = np.linspace(0, 1, len(test_data.get_entry_from_id(1)))
-#ax.plot(x_vals, test_data.get_entry_from_id(12))
-#ax.plot(x_vals, test_data.get_entry_from_id(50))
-#plt.show()
+#ax.plot(x_vals, test_data.get_entry_from_id(20))
+#ax.plot(x_vals, test_data.get_entry_from_id(29))
+
+
+plt.show()
 
 
