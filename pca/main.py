5c25050875a7d49df62b89ed15537e429619b80a
Daniel Alimadadian <danielalimadadian01@gmail.com>
2023-01-09 14:30:38 +0000

    Updated main.py

---
+++ pca/main.py
@@ -0,0 +1,14 @@
+import gui.gui as gui
+from tex_template.report_plot_funcs import PCAPlot
+from tex_template.report_generator import generate
+
+def main():
+    app = gui.Window()
+    app.mainloop()
+    pcaplot = PCAPlot().run_all(app.guidata)
+    generate(pcaplot)
+
+if __name__ == "__main__":
+    main()
+
+
