ac2c2d43f5c54ad6e13d55a633b29d0384290cc3
Daniel Alimadadian <danielalimadadian01@gmail.com>
2023-01-15 15:59:04 +0000

    Added dynamic ttest and scores plots

--- Metabolomics_ML/pca/PCA_2class.py
+++ Metabolomics_ML/pca/PCA_2class.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
+from matplotlib.patches import Ellipse
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from scipy import stats
@@ -197,25 +198,40 @@ class Data:
     
     def get_quantiles(self, loadings_matrix: pd.DataFrame, q: float=0.95):
         
-        #quantiles for PC1/PC2 currently
-        pc1, pc2 = [j[0] for i, j in self.loadings], [j[1] for i, j in self.loadings]
-        
         self.q = q
-        self._upper_quantile_pc1 = np.quantile(pc1, q)
-        self._lower_quantile_pc1 = np.quantile(pc1, 1-q)
 
-        self.pc1_loadings = [
-            (i, np.array([j[0], j[1]])) for i, j in self.loadings if j[0] > self._upper_quantile_pc1 or j[0] < self._lower_quantile_pc1
+        self._sig_labels_list = []
+        """
+        List of lists where each list represents all significant labels for that PC (index 0
+        represents PC1 etc.)
+        """
+
+        self._sig_loadings_labels = []
+        """
+        Labels of all significant loadings for all PCs chosen.
+        """
+
+        for i in range(1, self.n_components+1):
+            current_pc_labels, current_pc = np.array(loadings_matrix.index), loadings_matrix.loc[:,f'PC{i}'].to_numpy()
+            upper_quantile, lower_quantile = np.quantile(current_pc, self.q), np.quantile(current_pc, 1-self.q)
+            
+            # saving upper and lower quantiles for PC1 for plotting ranked loadings
+            if i == 1:
+                self._upper_quantile_pc1 = upper_quantile
+                self._lower_quantile_pc1 = lower_quantile
+
+            sig_labels = [
+                label for label, loading in zip(current_pc_labels, current_pc) if loading >= upper_quantile or loading <= lower_quantile
             ]
-        self.pc2_loadings = [
-            (i, np.array([j[0], j[1]])) for i, j in self.loadings if j[1] > np.quantile(pc2, q) or j[1] < np.quantile(pc2, 1-q)
-            ]
+
+            self._sig_labels_list.append(sig_labels)
+
+            # labels of loadings outside upper/lower quartiles for the current PC
+            self._sig_loadings_labels += sig_labels
+
         
-        self.sig_loadings = self.pc1_loadings + self.pc2_loadings
-        
-        self._pc1_loadings_labels = [i[0] for i in self.pc1_loadings]
-        self._pc2_loadings_labels = [i[0] for i in self.pc2_loadings]
-        self._sig_loadings_labels = self._pc1_loadings_labels + self._pc2_loadings_labels
+        # list of significant loadings: tuples in the form (label, np.array (of loadings)) 
+        self.sig_loadings = [loadings for loadings in self.loadings if loadings[0] in self._sig_loadings_labels]
 
         labels_column = [
             label if label in self._sig_loadings_labels else "" for label in self.labels
@@ -259,9 +275,8 @@ class Data:
         # initialise p_values DataFrame populated with zeros
         p_values = pd.DataFrame(np.zeros((len(data.columns[2:]), 3)), columns=['p-value', 'Significance', 'Bonferroni'], index=[data.columns[2:]])
 
-        # initialise PC1/PC2 stats dictionaries
-        pc1_stats = {}
-        pc2_stats = {}
+        # initialise stats list of dictionaries for all PCs - index 0 shows stats dict for PC1 etc.
+        pcs_stats = []
 
         ### iterating through pandas dataframe for t-test is slow - code to be optimised using numpy
 
@@ -270,48 +285,54 @@ class Data:
         self._case_data = data.query(f'Class == {self.case}')
         bonf_factor = len(data.columns[2:])
 
-        for i, col in enumerate(data.columns[2:]):
-
-            # initialise DataFrame for mean, SD, and SEM for each column
-            stat = pd.DataFrame(np.zeros((2, 3)), columns=['Mean', 'SD', 'SEM'], index=[self.control, self.case])
-
-            # set mean, SD, and SEM for control and case for each column
-            stat.loc[self.control, 'Mean'] = np.mean(self._control_data.loc[:, col])
-            stat.loc[self.case, 'Mean'] = np.mean(self._case_data.loc[:, col])
-            stat.loc[self.control, 'SD'] = np.std(self._control_data.loc[:, col])
-            stat.loc[self.case, 'SD'] = np.std(self._case_data.loc[:, col])
-            stat.loc[self.control, 'SEM'] = (stat.loc[self.control, 'SD'])/np.sqrt(self.num_control)
-            stat.loc[self.case, 'SEM'] = (stat.loc[self.case, 'SD'])/np.sqrt(self.num_case)
-
-            # store stats for top PC1/PC2 loadings - for plotting bar charts
-            if col in self._pc1_loadings_labels:
-                pc1_stats[col] = stat
-            elif col in self._pc2_loadings_labels:
-                pc2_stats[col] = stat
+        for pc_labels in self._sig_labels_list:
             
-            # run t-test
-            p_value = stats.ttest_ind_from_stats(mean1=stat.loc[self.control, 'Mean'], std1=stat.loc[self.control, 'SD'], nobs1=self.num_control,
-                                                mean2=stat.loc[self.case, 'Mean'], std2=stat.loc[self.case, 'SD'], nobs2=self.num_case)
+            # initialise dictionary for storing stats for each label
+            pc_stats = {}
+
+            for i, col in enumerate(data.columns[2:]):
+
+                # initialise DataFrame for mean, SD, and SEM for each column
+                stat = pd.DataFrame(np.zeros((2, 3)), columns=['Mean', 'SD', 'SEM'], index=[self.control, self.case])
+
+                # set mean, SD, and SEM for control and case for each column
+                stat.loc[self.control, 'Mean'] = np.mean(self._control_data.loc[:, col])
+                stat.loc[self.case, 'Mean'] = np.mean(self._case_data.loc[:, col])
+                stat.loc[self.control, 'SD'] = np.std(self._control_data.loc[:, col])
+                stat.loc[self.case, 'SD'] = np.std(self._case_data.loc[:, col])
+                stat.loc[self.control, 'SEM'] = (stat.loc[self.control, 'SD'])/np.sqrt(self.num_control)
+                stat.loc[self.case, 'SEM'] = (stat.loc[self.case, 'SD'])/np.sqrt(self.num_case)
+
+                # store stats for top loadings in current PC- for plotting bar charts
+                if col in pc_labels:
+                    pc_stats[col] = stat
+                
+                # run t-test
+                p_value = stats.ttest_ind_from_stats(mean1=stat.loc[self.control, 'Mean'], std1=stat.loc[self.control, 'SD'], nobs1=self.num_control,
+                                                    mean2=stat.loc[self.case, 'Mean'], std2=stat.loc[self.case, 'SD'], nobs2=self.num_case)
+                
+                if p_value[1] < 0.001:
+                    significance = '***'
+                elif p_value[1] < 0.01:
+                    significance = '**'
+                elif p_value[1] <0.05:
+                    significance = '*'
+                else:
+                    significance = 'NS'
+
+                if i == 0:
+                    p_values = pd.DataFrame([np.array([np.round(p_value[1], 6), significance, np.round(p_value[1] * bonf_factor, 6)])], columns=['p-value', 'Significance', 'Bonferroni'], index=[col])
+
+                else:    
+                    p_values.loc[col] = np.array([np.round(p_value[1], 6), significance, np.round(p_value[1] * bonf_factor, 6)])
             
-            if p_value[1] < 0.001:
-                significance = '***'
-            elif p_value[1] < 0.01:
-                significance = '**'
-            elif p_value[1] <0.05:
-                significance = '*'
-            else:
-                significance = 'NS'
-
-            if i == 0:
-                p_values = pd.DataFrame([np.array([np.round(p_value[1], 6), significance, np.round(p_value[1] * bonf_factor, 6)])], columns=['p-value', 'Significance', 'Bonferroni'], index=[col])
-
-            else:    
-                p_values.loc[col] = np.array([np.round(p_value[1], 6), significance, np.round(p_value[1] * bonf_factor, 6)])
+            # add this PCs stats to pcs_stats list
+            pcs_stats.append(pc_stats)
         
         if sort_p_values:
             p_values = p_values.sort_values(['p-value', 'Bonferroni'], key=lambda val: val.astype(float))
 
-        self.ttests = TTest(p_values=p_values, pc1_stats=pc1_stats, pc2_stats=pc2_stats)
+        self.ttests = TTest(p_values=p_values, pcs_stats=pcs_stats)
         
         return self.ttests
 
@@ -344,7 +365,7 @@ class Data:
         for upper and lower quartiles, and data labels for significant loadings (i.e above and 
         below the quartiles).
 
-         Parameters
+        Parameters
         ----------
         ranked_loadings: list
             List of tuples in the form (str, np.array)
@@ -360,7 +381,7 @@ class Data:
         ax.scatter(x_points, y_points, c='black', s=10, alpha=0.5)
 
         if labels:
-            for i in range(len(self.pc1_loadings)):
+            for i in range(len(self._sig_labels_list[0])):
                 ax.annotate(ranked_loadings[i][0], (x_points[i], y_points[i]), fontsize=3)
                 ax.annotate(ranked_loadings[-i-1][0], (x_points[-i-1], y_points[-i-1]), fontsize=3)
 
@@ -374,10 +395,12 @@ class Data:
             ax.plot(x_points, [self._upper_quantile_pc1 for i in x_points], c='red', linestyle='--')
             ax.plot(x_points, [self._lower_quantile_pc1 for i in x_points], c='red', linestyle='--')
         
-    def plot_scores(self, scores_matrix: pd.DataFrame, figure: tuple=None, fontsize: int=5):
+    def plot_scores(self, scores_matrix: pd.DataFrame, pcs: tuple=(1, 2), figure: tuple=None, hotelling: float=None, fontsize: int=5):
         """
-        Plots scores. If n_components is greater than 3, automatically plots only
-        the first 3 PCs.
+        Plots scores. By default, plots the first 2 PCs. Optional tuple input pcs allows you to choose
+        which PCs to plot (e.g. pcs=(1, 3) plots PC1 against PC3). Takes float hotelling as a parameter if you would like the
+        scores plot to contain Hotelling's T2 confidence interval around the scores
+        for each class. Float represents confidence interval.
         """
         colors = []
         # get class values from numpy to speed up run time
@@ -399,22 +422,26 @@ class Data:
         for handle in legend_elements:
             handle.set_linestyle("")
 
-        if self.n_components == 2:
-            self._plot_2d_scores(np_scores=np_scores, colors=colors, legend_elements=legend_elements, id_labels=id_labels, figure=figure, fontsize=fontsize)    
-        elif self.n_components >= 3:
-            self._plot_3d_scores(np_scores=np_scores, colors=colors, legend_elements=legend_elements, id_labels=id_labels, figure=figure, fontsize=fontsize)
+        self._plot_2d_scores(np_scores=np_scores, colors=colors, legend_elements=legend_elements, id_labels=id_labels, pcs=pcs, figure=figure, hotelling=hotelling, fontsize=fontsize)    
+        # elif self.n_components >= 3:
+        #     self._plot_3d_scores(np_scores=np_scores, colors=colors, legend_elements=legend_elements, id_labels=id_labels, figure=figure, fontsize=fontsize)
 
-    def _plot_2d_scores(self, np_scores, colors, legend_elements, id_labels, figure: tuple=None, fontsize: int=5):
+    def _plot_2d_scores(self, np_scores, colors, legend_elements, id_labels, pcs: tuple=(1, 2), hotelling: float=None, figure: tuple=None, fontsize: int=5):
         if figure is not None:
             fig, ax = figure
         else:  
             fig, ax = plt.subplots()
 
-        ax.scatter(np_scores[:, 1], np_scores[:, 2], c=colors, s=25, edgecolors='black', alpha=0.5)
+        first_pc, second_pc = pcs
+
+        ax.scatter(np_scores[:, first_pc], np_scores[:, second_pc], c=colors, s=25, edgecolors='black', alpha=0.5)
         for i, id in enumerate(id_labels):
-            ax.annotate(id, (np_scores[i, 1], np_scores[i, 2]), fontsize=fontsize)
-        self._add_labels_scores(ax, legend_elements)
-        
+            ax.annotate(id, (np_scores[i, first_pc], np_scores[i, second_pc]), fontsize=fontsize)
+        self._add_labels_scores(ax, legend_elements, pcs=pcs)
+
+        if hotelling:
+            self._plot_hotelling(np_scores, q=hotelling, figure=(fig, ax))
+      
     def _plot_3d_scores(self, np_scores, colors, legend_elements, id_labels, figure: tuple=None, fontsize: int=5):
         if figure is not None:
             fig, ax = figure
@@ -427,10 +454,13 @@ class Data:
         ### add text labels for 3d plot
     
     @staticmethod
-    def _add_labels_scores(ax, legend_elements):
+    def _add_labels_scores(ax, legend_elements, pcs: tuple):
+        
+        first_pc, second_pc = pcs
+
         ax.set_title('Scores Plot')
-        ax.set_xlabel('PC1')
-        ax.set_ylabel('PC2')
+        ax.set_xlabel(f'PC{first_pc}')
+        ax.set_ylabel(f'PC{second_pc}')
         ax.grid(linestyle='--')
         ax.legend(handles=legend_elements, loc='lower left', title='Classes', prop={'size': 8})
         if hasattr(ax, "set_zlabel"):
@@ -442,6 +472,13 @@ class Data:
         ax.set_xlabel('PC1')
         ax.set_ylabel('PC2')
         ax.grid(linestyle='--')
+    
+    @staticmethod
+    def _plot_hotelling(scores: np.ndarray, q: float, figure: tuple):
+        fig, ax = figure
+
+        pass
+
 
     def plot_vars(self, vars_array: np.ndarray, threshold: float=None, cumulative: bool=False, figure: tuple=None):
         rows = [f'PC{i}' for i in range(1, self.n_components+1)]
@@ -462,15 +499,53 @@ class Data:
             ax.set_ylabel("% of total variance")
 
     def plot_ttests(self, ttests: TTest):
-        pc1_stats = ttests.pc1_stats
-        pc2_stats = ttests.pc2_stats
         
-        fig, ax = plt.subplots()
+        # initialising the index of the first figure object
+        figure_number = 1
 
-        for feature_name, stats in pc1_stats.items():
-            ax.bar([self.original_control, self.original_case], [stats.loc[self.control, 'Mean'], stats.loc[self.case, 'Mean']], color=['green', 'blue'], yerr=[stats.loc[self.control, 'SEM'], stats.loc[self.case, 'SEM']], capsize=20)
-            ax.set_title(feature_name)
-            plt.show()
+        # looping through each PC, which is a dictionary of key-value pairs in the form:
+        # label: DataFrame of stats
+
+        for pc in ttests.pcs_stats:
+            # total number of plots
+            num_plots = 0
+            # get index of current plot
+            current_plot = 0
+            
+            while num_plots < len(pc):
+                fig = plt.figure(figure_number)
+                figure_number += 1
+                
+                for i in range(1, 10):
+                    if current_plot < len(pc.keys()):
+                        current_ax = fig.add_subplot(3, 3, i)
+
+                        # get stats for current plot
+                        feature_name = list(pc.keys())[current_plot]
+                        stats = pc[feature_name]
+
+                        current_ax.bar(
+                            [self.original_control, self.original_case], [stats.loc[self.control, 'Mean'], stats.loc[self.case, 'Mean']], color=['green', 'blue'], 
+                            edgecolor='black', alpha=0.5, yerr=[stats.loc[self.control, 'SEM'], stats.loc[self.case, 'SEM']], capsize=10
+                        )
+
+                        self._format_ttests(figure=(fig, current_ax), feature_name=feature_name, ttests=ttests)
+
+                        num_plots += 1
+                        current_plot += 1
+                
+                fig.tight_layout()
+            
+    
+    @staticmethod
+    def _format_ttests(figure: tuple[plt.Figure, plt.Axes], feature_name: str, ttests: TTest):
+        fig, ax = figure
+        ymin, ymax = ax.get_ylim()
+        ax.set_ylim(bottom=ymin, top=ymax*1.25)
+        ax.set_ylabel('Spectral Integral (AU)', fontsize=8)
+        ax.set_title(feature_name, fontsize=8)
+        ax.tick_params(which='both', labelsize=8)
+        ax.text(0.5, 0.85, ttests.p_values.loc[feature_name, 'Significance'], verticalalignment='top', horizontalalignment='center', fontsize=8, color='black', transform=fig.transFigure)
 
     @property
     def pca(self):
@@ -489,8 +564,7 @@ class Entry:
 @dataclass
 class TTest:
     p_values: pd.DataFrame
-    pc1_stats: dict
-    pc2_stats: dict
+    pcs_stats: list[dict]
 
 
 if __name__ == "__main__":
@@ -499,45 +573,16 @@ if __name__ == "__main__":
     # plots don't work without class_labels dict - cannot append strings into numpy array
     test_data.set_dataset_classes(control='RRMS', case='SPMS', class_labels={'control': -1, 'case': 1})
 
-    ### where to put self.n_components? Currently a class property.
-    loadings_matrix, scores_matrix, vars_array = test_data.get_loadings(n_components=20), test_data.get_scores(), test_data.get_vars(ratio=True)
-
-    loadings_matrix = test_data.get_quantiles(loadings_matrix)
-
-    #test_data.plot_loadings(loadings_matrix)
-
-    # quantiles_matrix = test_data.get_quantiles(loadings_matrix)
-
+    
+    test_data.get_loadings(n_components=1)
+    test_data.get_scores()
+    test_data.get_vars(ratio=True)
+    test_data.get_quantiles(test_data.loadings_matrix, q=0.95)
     test_data.rank_loadings()
-    test_data.get_sig_data()
+    test_data.run_ttests()
 
-    ttests = test_data.run_ttests()
+    # test_data.plot_scores(test_data.scores_matrix, pcs=(1, 3))
 
-    # test_data.plot_ttests(ttests)
-
-    test_data.plot_vars(vars_array=vars_array, threshold=95, cumulative=True)
-    # test_data.plot_loadings(quantiles_matrix, sig_labels=True)
-    # test_data.plot_scores(scores_matrix)
-
-    #print(scores_matrix.iloc[:,1:] @ loadings_matrix.iloc[:, :2].T)
-    #print(test_data.scaled_test_data.iloc[:, 2:] - (scores_matrix.iloc[:,1:] @ loadings_matrix.iloc[:, :2].T))
-
-    #x_data, y_data = test_data._split_data()
-    #print(pd.DataFrame(StandardScaler().fit_transform(x_data)))
-
-    ### TO DO
-    # check out quantiles and ranked loadings
-    # clean up all plots
-    # create file with questions + next steps
-
-    #### General rule - using get methods outputs pandas dataframes, but attributes use numpy arrays
-    ### ttests in pandas dataframes due to use of strings e.g. significance level
-
-    # plot spectrum
-    #fig, ax = plt.subplots()
-    #x_vals = np.linspace(0, 1, len(test_data.get_entry_from_id(1)))
-    #ax.plot(x_vals, test_data.get_entry_from_id(20))
-    #ax.plot(x_vals, test_data.get_entry_from_id(29))
-
+    test_data.plot_ttests(test_data.ttests)
 
     plt.show()
