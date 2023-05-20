d73dc4b37575967ed05c649841197273918b150f
Daniel Alimadadian <danielalimadadian01@gmail.com>
2023-05-20 23:12:42 +0100

    Add mean decrease in accuracy metric

--- Metabolomics_ML/validation/crossval.py
+++ Metabolomics_ML/validation/crossval.py
@@ -2,8 +2,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold, train_test_split
+from sklearn.model_selection import KFold, ShuffleSplit
+from sklearn.utils import resample
 from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.inspection import permutation_importance
+from sklearn.cross_decomposition import PLSRegression
 from typing import Union
 import random
 
@@ -63,7 +66,7 @@ class CrossValidation:
     def shuffle(data: pd.DataFrame):
         return data.sample(frac=1)
 
-    def external_cv(self, data: pd.DataFrame, perm_test: bool=True):
+    def external_cv(self, data: pd.DataFrame, perm_test: bool, mda: bool):
         """
         Optional perm_test input - carries out permutation test. Adds results of each permutation test
         to Metrics object.
@@ -72,7 +75,7 @@ class CrossValidation:
 
         metrics = []
 
-        for train_index, test_index in kf.split(data):
+        for i, (train_index, test_index) in enumerate(kf.split(data), 1):
             
             # for each fold, split data into training and test set
             train_data_, test_data_ = data.iloc[train_index], data.iloc[test_index]
@@ -84,11 +87,17 @@ class CrossValidation:
                 train_set, test_set = self._create_train_test(train_data_, test_data_, _from_perm_data=False)
                 
             # build model on training set, and test using test set, storing the metrics 
-            metrics.append(self.build_model(train_set, test_set, perm_test))
+            metrics.append(self.build_model(train_set, test_set, perm_test, mda))
+            # print(f"Fold {i} complete.")
 
         return metrics
 
-    def run_all(self, perm_test: bool=True):
+    def run_all(self, perm_test: bool=True, mda: bool=True):
+        """
+        Takes two boolean parameters: perm_test determines whether permutation test with 
+        random class allocations is desired, and mda runs a mean decrease in accuracy on 
+        each feature. 
+        """
 
         if self.metrics is None:
             self.metrics = []
@@ -98,7 +107,7 @@ class CrossValidation:
                 self.add_random_labels(self.estimator)
             equal_data = self.equalise_classes(self.estimator)
             equal_data = self.shuffle(equal_data)
-            self.metrics.append(self.external_cv(equal_data, perm_test=perm_test))
+            self.metrics.append(self.external_cv(equal_data, perm_test=perm_test, mda=mda))
 
             print(f"Repetition {i+1} complete.")
         
@@ -141,7 +150,9 @@ class CrossValidation:
 
         return train_set, test_set
 
-    def build_model(self, train_set: Union[OPLSData, PLSData], test_set: Union[OPLSData, PLSData], perm_test: bool):
+    def build_model(self, train_set: Union[OPLSData, PLSData], test_set: Union[OPLSData, PLSData], perm_test: bool, mda: bool):
+        
+        y_true = np.array([entry.class_ for entry in test_set.entries])
         
         if type(train_set) is OPLSData:
             # initialises opls attribute
@@ -155,8 +166,10 @@ class CrossValidation:
 
             y = train_set.pls.predict(test_set.scaled_data)
 
-        y_true = np.array([entry.class_ for entry in test_set.entries])
-        
+            if mda:
+                mda_dict = self.mean_decrease_acc(train_set, test_set, y_true)
+
+
         if test_set.control > test_set.case:
             y_pred = np.array([
                 test_set.control if val > (test_set.control + test_set.case) / 2 else test_set.case for val in y
@@ -169,6 +182,7 @@ class CrossValidation:
         # confusion matrix in the form:
         #     [ [TP, FP],
         #       [FN, TN] ]
+
         conf_matrix = confusion_matrix(y_true=y_true, y_pred=y_pred, labels=[test_set.case, test_set.control]).T
 
         accuracy = accuracy_score(y_true, y_pred)
@@ -182,7 +196,20 @@ class CrossValidation:
             rand_accuracy = accuracy_score(y_true, y_rand)
             rand_sensitivity = sensitivity_score(rand_conf_matrix)
             rand_specificity = specificity_score(rand_conf_matrix)
-
+            
+            if mda:
+                return Metrics(
+                conf_matrix=conf_matrix,
+                accuracy=accuracy,
+                sensitivity=sensitivity,
+                specificity=specificity,
+                rand_conf_matrix=rand_conf_matrix,
+                rand_accuracy=rand_accuracy,
+                rand_sensitivity=rand_sensitivity,
+                rand_specificity=rand_specificity,
+                mda=mda_dict
+                )
+            
             return Metrics(
             conf_matrix=conf_matrix,
             accuracy=accuracy,
@@ -215,34 +242,103 @@ class CrossValidation:
 
         return rand_scaled_data
 
+    def mean_decrease_acc(self, train_set: Union[OPLSData, PLSData], test_set: Union[OPLSData, PLSData], y_true: np.ndarray):
+        
+        # initialise dictionary for storing accuracies with associated labels permuted
+        mda_dict = {}
+
+        for label in train_set.labels:
+            permuted_data = train_set.scaled_test_data.copy()
+
+            # randomly shuffle each column
+            permuted_data[label] = np.random.permutation(permuted_data[label])
+
+            # retrain model
+            if type(train_set) is PLSData:
+                perm_train_set = PLSData.new_from_df(permuted_data)
+                perm_train_set.scaled_test_data = perm_train_set.test_data.copy()
+                perm_train_set._scaled_data = perm_train_set.scaled_test_data.iloc[:, 1:].to_numpy()
+                
+                # initialise pls property
+
+                perm_train_set.get_scores()
+                y_perm = perm_train_set.pls.predict(test_set.scaled_data)
+
+
+            if type(train_set) is OPLSData:
+                perm_train_set = OPLSData.new_from_df(permuted_data)
+
+
+            
+            if test_set.control > test_set.case:
+                y_perm = np.array([
+                test_set.control if val > (test_set.control + test_set.case) / 2 else test_set.case for val in y_perm
+            ])
+            else:
+                y_perm = np.array([
+                test_set.control if val < (test_set.control + test_set.case) / 2 else test_set.case for val in y_perm
+                ])
+            
+
+            mda_dict[label] = accuracy_score(y_true, y_perm)
+
+        return mda_dict
+
+    @staticmethod
+    def get_mda_data(metrics: list[list[Metrics]], sort: bool=True):
+        pass
+    
+    @staticmethod
+    def get_metric_means(metrics: list[list[Metrics]]):
+        
+        accs = []
+        spec = []
+        sens = []
+        rand_accs = []
+        rand_spec = []
+        rand_sens = []
+        mdas = {}    
+
+        for metric_list in metrics:
+            for metric in metric_list:
+                accs.append(metric.accuracy)
+                spec.append(metric.specificity)
+                sens.append(metric.sensitivity)
+                if metric.rand_accuracy:
+                    rand_accs.append(metric.rand_accuracy)
+                    rand_spec.append(metric.rand_specificity)
+                    rand_sens.append(metric.rand_sensitivity)
+                if metric.mda:
+                    mdas[0]
+
 if __name__ == "__main__":
     test_data = OPLSData.new_from_csv(r"C:\Users\mfgroup\Documents\Daniel Alimadadian\Metabolomics_ML\tests\test_data.csv")
-    test_data.set_dataset_classes(control="RRMS", case="SPMS", class_labels={'control': -1, 'case': 1}, sort=True)
-    cv = CrossValidation(estimator=test_data, folds=8, repetitions=50)
+    test_data.set_dataset_classes(control='RRMS', case='SPMS', class_labels={'control': -1, 'case': 1}, sort=True)
 
-    metrics = cv.run_all(perm_test=True)
+    cv = CrossValidation(estimator=test_data, folds=10, repetitions=5)
+
+    metrics = cv.run_all(perm_test=True, mda=False)
     accs = []
     spec = []
     sens = []
     rand_accs = []
+    # mda = []
     for metric_list in metrics:
         for metric in metric_list:
             accs.append(metric.accuracy)
             spec.append(metric.specificity)
             sens.append(metric.sensitivity)
             rand_accs.append(metric.rand_accuracy)
-    
+            # mda.append(metric.mda)
+        
     print(np.mean(accs))
     print(np.mean(rand_accs))
-    # print([entry.class_ for entry in train_set.entries])
+    # print(mda)
 
-    # self.test_data['Class'].value_counts()[self.control]
+    # print(test_data.scaled_test_data.iloc[:, :2])
+    # test_data.scaled_test_data['X.0.80....0.82.'] = np.random.permutation(test_data.scaled_test_data['X.0.80....0.82.'])
+    # print(test_data.scaled_test_data.iloc[:, :2])
 
-    
-
-    # print(test_data.pls.predict(test_data.test_data.iloc[:, 1:]))
-
-    
 
 
  
